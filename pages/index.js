60a9a647f7cc3fa890ca2980a98f91a4ecbb7c26
Mia de Búrca <mia.deburca@99designs.com>
2019-10-19 20:04:37 +1100

    Fix case specific issue

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Form from "../components/form";
 import Page from "../components/page";
-import Paper from "@material-ui/core/paper";
-import Container from "@material-ui/core/container";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 
 const Home = () => (
   <Page title="Apocalyspo">
