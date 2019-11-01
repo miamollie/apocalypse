14cefd8f56dac043f86a2f90ef35488ac019bed6
Mia de Búrca <mia.deburca@99designs.com>
2019-11-01 15:45:43 +1100

    Build out expanding UI

--- components/diagnosisBlock.js
+++ components/diagnosisBlock.js
@@ -1,24 +1,61 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
-const DiagnosisBlock = ({ test, result }) => {
-    const copy = {
-        stopBang: 'A message about a positive result from the stop bang thing',
-        bmi: 'bmi...',
-        thirdTest: 'derp derp derp derp',
+const useStyles = makeStyles({
+    avatar: props => ({
+        // backgroundColor: props.result ? 'green' : 'red',
+        backgroundColor: 'red',
+    }),
+});
+
+const DiagnosisBlock = props => {
+    const classes = useStyles(props);
+    const { test, result } = props;
+    const testDescription = {
+        stopBang:
+            'Aerobic activity arthritis dermatologist glucose hyperopia insulin iv laxatives lymph node mucus nausea stethoscope violence fluoxetine nardil pexeva quetiapine sertraline farad osmol pipe. ',
+        bmi:
+            'Antibiotics corticosteroids islet cells pulmonary strep screen white blood cells bar curie grain osmol wey.',
+        example:
+            'Allergen ct scan or cat scan ketones fluvoxamine guanfacine imipramine pamoate ludiomil methylphenidate nortriptyline tranylcypromine megadeath point tesla. Allergy shots and immunotherapy antihistamines dermatologist dust mites eeg (electroencephalogram) exhale gastroenteritis islet cells junk food lacrimal glands nearsighted stethoscope adderall clozaril duloxetine escitalopram nortriptyline paliperidone phenelzine quetiapine sertraline barleycorn furlong hand oersted therblig. ',
     }[test];
 
-    // how to indicate pass/fail in the ui?
+    const suggestedAction = {
+        stopBang:
+            'Aerobic activity arthritis dermatologist glucose hyperopia insulin iv laxatives lymph node mucus nausea stethoscope violence fluoxetine nardil pexeva quetiapine sertraline farad osmol pipe. ',
+        bmi:
+            'Antibiotics corticosteroids islet cells pulmonary strep screen white blood cells bar curie grain osmol wey.',
+        example:
+            'Allergen ct scan or cat scan ketones fluvoxamine guanfacine imipramine pamoate ludiomil methylphenidate nortriptyline tranylcypromine megadeath point tesla. Allergy shots and immunotherapy antihistamines dermatologist dust mites eeg (electroencephalogram) exhale gastroenteritis islet cells junk food lacrimal glands nearsighted stethoscope adderall clozaril duloxetine escitalopram nortriptyline paliperidone phenelzine quetiapine sertraline barleycorn furlong hand oersted therblig. ',
+    }[test];
+
+    const testName = {
+        stopBang: 'Stop Bang',
+        bmi: 'BMI – Body Mass Index',
+        example: 'Third Example',
+    }[test];
+
+    const subheader = result ? 'Patient is at risk' : 'No risk determined';
+
     return (
         <Card>
+            <CardHeader
+                title={testName}
+                subheader={subheader}
+                avatar={<Avatar classes={classes.avatar} />}
+            />
             <CardContent>
-                <Typography color="textSecondary" gutterBottom>
-                    {copy}
-                    <br />
-                    {result}
+                <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+                    {testDescription}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+                    {suggestedAction}
                 </Typography>
             </CardContent>
         </Card>
--- pages/result.js
+++ pages/result.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Page from '../components/page';
 import DiagnosisBlock from '../components/diagnosisBlock';
 import { useRouter } from 'next/router';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Link from 'next/link';
+import Grid from '@material-ui/core/Grid';
 
 const Result = () => {
     const router = useRouter();
@@ -10,13 +17,72 @@ const Result = () => {
 
     return (
         <Page title="Result">
-            {!Object.keys(router.query).length &&
-                'No result... try searching or add a new patient Here'}
-            {/* periop in a dropdown e.g. recommended tests. Open if Exists data, closed otherwise. */}
-            {stopBang && <DiagnosisBlock result={stopBang} test="stopBang" />}
-            {bmi && <DiagnosisBlock result={bmi} test="bmi" />}
-            {example && <DiagnosisBlock result={example} test="example" />}
-            {/* other stuff relevant to operating theatre in a dropdown */}
+            {!Object.keys(router.query).length ? (
+                <Typography>
+                    No result. Submit new patient
+                    <Link href="/">
+                        <a>here</a>
+                    </Link>
+                </Typography>
+            ) : (
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <ExpansionPanel>
+                            <ExpansionPanelSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography variant="h5" gutterBottom>
+                                    Preop considerations
+                                </Typography>
+                                {/* https://material-ui.com/components/badges/  */}
+                                {/* TODO include pass fail count in summary? */}
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Grid container spacing={3}>
+                                    <Grid item xs={12}>
+                                        <DiagnosisBlock result={stopBang} test="stopBang" />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <DiagnosisBlock result={bmi} test="bmi" />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <DiagnosisBlock result={example} test="example" />
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <ExpansionPanel>
+                            <ExpansionPanelSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2a-content"
+                                id="panel2a-header"
+                            >
+                                <Typography variant="h5" gutterBottom>
+                                    Postop considerations
+                                </Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Grid container spacing={3}>
+                                    <Grid item xs={12}>
+                                        <DiagnosisBlock result={stopBang} test="stopBang" />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <DiagnosisBlock result={bmi} test="bmi" />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <DiagnosisBlock result={example} test="example" />
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                    </Grid>
+                </Grid>
+            )}
         </Page>
     );
 };
