9fc59df9385572fa206a88e4ddedf2ec8bc9645b
Mia de Búrca <mia.deburca@99designs.com>
2019-11-01 11:57:32 +1100

    Update nav WIP

--- components/page.js
+++ components/page.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Head from 'next/head';
 import Nav from '../components/nav';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Page = ({ title, children }) => (
     <>
         <Head>
             <title>Apocalyso {title && `- ${title}`}</title>
         </Head>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                    {/* TODO menu should open as Nav */}
+                </IconButton>
+                <Typography variant="h6">Apocalyso</Typography>
+                <Button color="inherit">
+                    <SearchIcon />
+                    Search
+                </Button>
+            </Toolbar>
+        </AppBar>
         <Nav />
         <main>{children}</main>
     </>
